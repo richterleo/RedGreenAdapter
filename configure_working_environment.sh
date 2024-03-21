3f61c6bc49cf4df386c9c27b4544475714f601fa
richterleo <richter.leo94@gmail.com>
2024-03-21 16:35:11 +0000

    added testing for accelerate

--- configure_working_environment.sh
+++ configure_working_environment.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Prevent auto tmux
+touch ~/.no_auto_tmux
+
 # Create directory for Miniconda and download the installer
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
