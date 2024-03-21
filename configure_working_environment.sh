49c612bfbfcaea79a50b02829077312001c6d5a3
richterleo <richter.leo94@gmail.com>
2024-03-21 10:53:10 +0000

    standardize defining vm

---
+++ configure_working_environment.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Create directory for Miniconda and download the installer
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+
+# Install Miniconda
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+
+# Remove the installer script
+rm -rf ~/miniconda3/miniconda.sh
+
+# Initialize Conda for Bash shell
+~/miniconda3/bin/conda init bash
+
+# Source the bashrc to refresh the environment
+source ~/.bashrc
+
+# Create a Conda environment
+echo "Creating conda environment"
+~/miniconda3/bin/conda create env -f environment.yml
+
+# Add conda activation command to bashrc
+echo "conda activate redgreen" >> ~/.bashrc
+
+# Activate the Conda environment
+source ~/miniconda3/bin/activate redgreen
+
