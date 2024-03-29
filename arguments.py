46ff0928b5534d8db3cb53e6591f8d8e262ac46f
richterleo <richter.leo94@gmail.com>
2024-03-29 16:39:16 +0000

    change vs code debugger launch

--- arguments.py
+++ arguments.py
@@ -80,7 +80,7 @@ training_args = {
     "max_steps": 1000,
     "learning_rate": 1e-3,
     "gradient_accumulation_steps" :1,
-    "logging_steps": 10,
+    "logging_steps": 100,
     "eval_steps" :500,
     "output_dir":"dpo_anthropic_hh",
     "warmup_steps" :150,
