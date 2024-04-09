dbe2878c763cf0e311373eaf5e8a78e6f8d09dc8
richterleo <richter.leo94@gmail.com>
2024-04-09 09:36:32 +0000

    BOG as class with evaluation

--- bag_of_words.py
+++ bag_of_words.py
@@ -4,17 +4,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.utils import shuffle
 
+import numpy as np
+
 
 from utils import get_hh, build_new_dataset, time_block, preprocess_sample
 
 
 class BOG:
     
-    def __init__(self, sanity_check=True):
+    def __init__(self, sanity_check=True, random_state=0):
         
         self.vectorizer = CountVectorizer()
         self.model = LogisticRegression(max_iter=1000)
         self.sanity_check=sanity_check
+        self.random_state = random_state
     
     def train(self):
            
@@ -32,12 +35,12 @@ class BOG:
 
         assert X_train.shape[0] == len(preprocessed_train_dataset['label'])
 
-        X_train, y_train = shuffle(X_train, preprocessed_train_dataset['label'], random_state=0)
+        X_train, y_train = shuffle(X_train, preprocessed_train_dataset['label'], random_state=self.random_state)
 
         # train logistic regression classifier
         self.model.fit(X_train, y_train)
         
-    def eval(self):
+    def eval(self, num_samples=0):
         
         eval_dataset = get_hh("test", sanity_check=self.sanity_check)
         
@@ -49,16 +52,37 @@ class BOG:
             preprocessed_test_dataset = unraveled_eval_dataset.map(preprocess_sample)
             
         X_test = self.vectorizer.transform(preprocessed_test_dataset['text'])
+        y_test = preprocessed_test_dataset['label']
         
-        assert X_test.shape[0] == len(preprocessed_test_dataset['label'])
+        assert X_test.shape[0] == len(y_test)
         
         y_pred = self.model.predict(X_test)
         
-        print("Accuracy:", accuracy_score(preprocessed_test_dataset['label'], y_pred))
-        print("Classification Report:\n", classification_report(preprocessed_test_dataset['label'], y_pred))
+        print("Accuracy:", accuracy_score(y_test, y_pred))
+        print("Classification Report:\n", classification_report(y_test, y_pred))
         
+        if num_samples > 0:
+            # Choose multiple random samples to display
+            sample_indices = np.random.sample(range(len(y_test)), num_samples)  # Generate random indices without replacement
+
+            print("Random Test Samples:")
+            for idx in sample_indices:
+                sample_text = preprocessed_test_dataset['text'][idx]
+                sample_predicted_label = y_pred[idx]
+                sample_actual_label = y_test[idx]
+
+                # Print the sample
+                print("\nSample Text:", sample_text)
+                print("Predicted Label:", sample_predicted_label)
+                print("Actual Label:", sample_actual_label)
+            
         
-        
+
+if __name__ == "__main__":
+    
+    bog = BOG()
+    bog.train()
+    bog.eval(num_samples=10)
         
 
 
